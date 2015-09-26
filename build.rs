3efaa68615e30397324fb2410a8a9824a30c97be
Elliott Rezny <erezny@gmail.com>
2015-09-25 19:06:21 -0500

    Cleanup

--- build.rs
+++ build.rs
@@ -9,24 +9,16 @@ use std::fs;
 use bindgen::{Bindings, BindgenOptions, LinkType, Logger};
 
 fn main() {
-   let out_dir = env::var("OUT_DIR").unwrap();
-   let lib_dir = Path::new(&out_dir).join("lib/");
-   let dest_path = Path::new(&lib_dir).join("xiapi.rs");
+    let out_dir = env::var("OUT_DIR").unwrap();
+    let lib_dir = Path::new(&out_dir).join("lib/");
+    let dest_path = Path::new(&lib_dir).join("xiapi.rs");
 
-   fs::create_dir(&lib_dir);
-
-   //let dest_path_str = dest_path.to_str().expect("dest_path invalid");
-
-    // println!("Dest Path: {}", dest_path_str);
-    // //environment specific
-    //
-    //
+    fs::create_dir(&lib_dir);
 
     let mut bindings = bindgen::builder();
     bindings.forbid_unknown_types();
 
-
-
+     // //environment specific
     let h_path = String::from("/Library/Frameworks/m3api.framework/Headers/xiApi.h");
 
     bindings.link_framework("m3api");
@@ -38,32 +30,6 @@ fn main() {
     let bindings = bindings.generate();
     let bindings = bindings.unwrap();
     bindings.write_to_file(&dest_path).unwrap();
-    // let output = Command::new("../rust-bindgen/target/debug/bindgen")
-    //         .args(&["-l", "m3api"])
-    //         .args(&["-match", "m3Identify.h", "-match" , "xiApi.h"])
-    //         .args(&["-match", "wintypedefs.h"])
-    //         .args(&["-o", dest_path_str])
-    //         .args(&[ "/Library/Frameworks/m3api.framework/Headers/xiApi.h"])
-    //         .output().unwrap_or_else(|e| {
-    //              panic!("failed to execute process: {}", e)
-    //          });
-    //
-    // println!("status: {}", output.status);
-    // println!("stdout: {}", String::from_utf8_lossy(&output.stdout));
-    // println!("stderr: {}", String::from_utf8_lossy(&output.stderr));
-    //
-    // let rust_output = Command::new("rustc")
-    //     .args(&["--out-dir", &out_dir])
-    //     .args( &[dest_path_str])
-    //     .output().unwrap_or_else( |e| {
-    //         panic!("failed to execute process: {}", e)
-    //         });
-    //
-    //
-    // println!("status: {}", rust_output.status);
-    // println!("stdout: {}", String::from_utf8_lossy(&rust_output.stdout));
-    // println!("stderr: {}", String::from_utf8_lossy(&rust_output.stderr));
-
 
     println!("cargo:include={}", &dest_path.to_str().unwrap());
     println!("cargo:rustc-link-lib=framework=m3api");
---
+++ readme.md
@@ -0,0 +1,41 @@
+xiapi_sys-rust
+============
+
+A bindgen binding for Ximea xiApi.
+
+Requirements
+------------
+
+* clang 3.4 and up
+
+Note: The libclang.so has to be statically linked with LLVM or you will
+encounter [issue 89][]. You can also use LD_PRELOAD=/path/to/libclang.so to
+workaround the problem.
+
+Building
+--------
+
+    $ cargo build
+
+Note: This links with Apple's version of libclang on OS X by default. This can be changed by setting the LIBCLANG_PATH environment variable.
+
+If you are running the command line tool you will also need to append this
+path to your DYLD_LIBRARY_PATH environment variable, which you might already have set if you have installed the Rust compiler outside of standard /usr/local path.
+
+The default path on OS X is:
+
+    /Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/lib/
+
+Or if you only have Xcode Command Line Tools installed:
+
+    export DYLD_LIBRARY_PATH=/Library/Developer/CommandLineTools/usr/lib
+
+Examples
+--------
+
+see examples/
+
+TODO
+----
+
+* use pkg_config or similar for xiAPI location.
